1d5daf63fc9d7ff761886c61fde37cc6da4eff39
harris <harriskwong120898@gmail.com>
2023-07-21 19:29:32 -0400

    Created food app, food model, made migrations to database

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
-    return render(request,'home/welcome.html',{'now': datetime.now()})
+    return render(request,'home/welcome.html',{'today': datetime.today()})
+
+#redirects user to admin login form if not logged in
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request,'home/authorized.html',{})
