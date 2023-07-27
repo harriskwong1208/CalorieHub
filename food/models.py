547b7451e9b29392e52522ebd0b395a6c5c14903
harris <harriskwong120898@gmail.com>
2023-07-26 20:26:32 -0400

    Made changes to food model data, added csrf token to food form.html

--- food/models.py
+++ food/models.py
@@ -5,4 +5,3 @@ class Food(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(blank=True)
     calories = models.IntegerField()
-    created = models.DateTimeField(auto_created=True)
