b06ead0b059e2fa9d9b06d9d7f0087b0c79fcc05
harris <harriskwong120898@gmail.com>
2023-07-26 20:49:01 -0400

    Customized form

--- food/forms.py
+++ food/forms.py
@@ -7,6 +7,18 @@ class FoodForm(forms.ModelForm):
     class Meta:
         model = Food
         fields = ['title','text','calories']
+        style = 'display: block'
+        widgets ={
+            'title': forms.TextInput(attrs={'style': style}),
+            'text': forms.Textarea(attrs={'style': style}),
+            'calories': forms.TextInput(attrs={'style': style}),
+
+        }
+        labels = {
+            'title':'Enter food name ',
+            'text': 'Enter description of food ',
+            'calories': 'Calorie Count '
+        }
 
     # def clean_title(self):
     #     title= self.cleaned_data['title']
