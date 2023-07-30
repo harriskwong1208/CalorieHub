6114a472d6280c50699a3c574a025b1f3d022d11
harris <harriskwong120898@gmail.com>
2023-07-29 20:30:16 -0400

    Updated createview in food views to save newly created form to curretnly logged in user.

--- food/views.py
+++ food/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-
+from django.http.response import HttpResponseRedirect
 from django.views.generic import CreateView,DetailView,ListView,UpdateView
 
 #For delete views 
@@ -14,24 +14,39 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Food
 from .forms import FoodForm
 
-class FoodDeleteView(DeleteView):
+class FoodDeleteView(LoginRequiredMixin,DeleteView):
     model = Food
     success_url = '/food/'
     template_name = 'food/food_delete.html'
+    login_url = '/admin'
 
-class FoodUpdateView(UpdateView):
+class FoodUpdateView(LoginRequiredMixin,UpdateView):
     model = Food
     form_class = FoodForm
     success_url = '/food/'    
+    login_url = '/admin'
 
-class FoodCreateView(CreateView):
+class FoodCreateView(LoginRequiredMixin,CreateView):
     model = Food
     #imported from forms.py
     form_class = FoodForm
 
     #Does the same thing as above
     # fields = ['title','text','calories']
-    success_url = '/food/'
+    login_url = '/admin'
+    success_url = '/food'
+
+    # When a user submits a form, this method is called. 
+    # It takes the form data, associates it with the currently 
+    # logged-in user (assuming the user is authenticated), 
+    # saves it to the database, and then redirects the user to a 
+    # specified URL upon successful submission.
+    def form_valid(self,form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
 
 #LoginRequireMIxin = only list food items that belows to the user
 class FoodListView(LoginRequiredMixin,ListView):
@@ -53,9 +68,10 @@ class FoodListView(LoginRequiredMixin,ListView):
 
 
 
-class FoodDetailView(DetailView):
+class FoodDetailView(LoginRequiredMixin,DetailView):
     model = Food
     context_object_name = "food"
+    login_url = '/admin'
 
 
 #function based view, does the same thing as above
