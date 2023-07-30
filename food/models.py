a5766bda4a416f13eeccb53ed6363403f433c952
harris <harriskwong120898@gmail.com>
2023-07-29 20:04:59 -0400

    Added foreign key to food model.

--- food/models.py
+++ food/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+#import user model 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
 class Food(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(blank=True)
     calories = models.IntegerField()
+
+    #Set up foreign key.
+    #on_delete=models.CASCADE = all food info gets deleted 
+    #if user assiocated with that data gets deleted
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="food")
--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     #Using class-based view
-    path('home',views.HomeView.as_view()),
+    path('home',views.HomeView.as_view(),name="home"),
 
     path('authorized',views.AuthorizedView.as_view()),
 ]
